8fba31c051d366120dc6f7b9a12427197fa7bcfb
Bamarcheti <bmarchetifiorin@gmail.com>
2022-12-04 16:05:57 -0400

    Adicionar arquivos de projeto.

---
+++ PROVA_DEV2/Pages/Clientes/lista_clientes.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PROVA_DEV2.Pages.Clientes
+{
+    public class lista_clientesModel : PageModel
+    {
+        public List<ClientInfoNovo> listClients = new List<ClientInfoNovo>();
+        public void OnGet()
+        {
+            try
+            {
+                string connectionstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=prova_dev2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    String sql = "Select * from clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfoNovo clientInfo = new ClientInfoNovo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.adress = reader.GetString(4);
+                                clientInfo.create_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCE��O : " + ex.Message.ToString());
+            }
+        }
+    }
+
+    public class ClientInfoNovo
+    {
+        public string id;
+        public string name;
+        public string email;
+        public string phone;
+        public string adress;
+        public string create_at;
+    }
+}
---
+++ PROVA_DEV2/Pages/Filmes/lista_filmes.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PROVA_DEV2.Pages.Filmes
+{
+    public class lista_filmesModel : PageModel
+    {
+        public List<FilmsInfoNovo> listFilms = new List<FilmsInfoNovo>();
+        public void OnGet()
+        {
+            try
+            {
+                string connectionstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Films_db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    String sql = "Select * from films";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                FilmsInfoNovo filmInfo = new FilmsInfoNovo();
+                                filmInfo.Id = "" + reader.GetInt32(0);
+                                filmInfo.name = reader.GetString(1);
+                                filmInfo.genre = reader.GetString(2);
+                                filmInfo.create_at = reader.GetDateTime(3).ToString();
+
+                                listFilms.Add(filmInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCE��O : " + ex.Message.ToString());
+            }
+        }
+    }
+
+    public class FilmsInfoNovo
+    {
+        public string Id;
+        public string name;
+        public string genre;
+        public string create_at;
+    }
+}
